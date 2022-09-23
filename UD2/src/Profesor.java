9562a84854139f9f45808a0ea4c16cc074d3e64d
DAM2Alu12 <DAM2Alu12@B01-PC12.IESGUARA.LOCAL>
2022-09-23 12:37:48 +0200

    Se empieza el UD2_8 -> meter en un array,luego meterlo al fichero y leer

---
+++ UD2/src/Profesor.java
@@ -0,0 +1,42 @@
+
+public class Profesor {
+	//Atributos
+	private String Nombre;
+	private double antiguedad;
+	
+	
+	//Constructor
+	public Profesor() {
+		
+	}
+	
+	public Profesor(String nombre, double antiguedad) {
+		super();
+		Nombre = nombre;
+		this.antiguedad = antiguedad;
+	}
+	//Getters and setters
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public double getAntiguedad() {
+		return antiguedad;
+	}
+	public void setAntiguedad(double antiguedad) {
+		this.antiguedad = antiguedad;
+	}
+	//Metodos
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Profesor [Nombre=" + Nombre + ", antiguedad=" + antiguedad + "]";
+	}
+	
+	
+	
+}
